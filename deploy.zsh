83fc487a0d959c41b8f3f5181114914b9f1479ab
Hans Kuijpers <hans2103@gmail.com>
2020-06-05 20:06:23 +0200

    disable packagist

--- deploy.zsh
+++ deploy.zsh
@@ -11,8 +11,8 @@ git clone $(git remote get-url origin) public
 cd public/
 
 # Who am I?
-git config user.email "${GIT_USER_EMAIL}"
-git config user.name "${GIT_USER_NAME}"
+git config user.email
+git config user.name
 
 git checkout gh-pages
 if [ 0 != $? ]; then
